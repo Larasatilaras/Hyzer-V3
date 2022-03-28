1f692617198a01a5c093797b3ff2ee42e30acb15
Laras <96417338+Larasatilaras@users.noreply.github.com>
2022-03-28 15:45:30 +0700

    Update viadana.js

--- plugins/viadana.js
+++ plugins/viadana.js
@@ -2,10 +2,10 @@ let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
 ╠═〘 PEMBAYARAN 〙 ═
 ╠═ Untuk Pembayaran Via Gopay
-╠➥ *6287892711054*
+╠➥ *6285759662798*
 ║- Owner Bot:
-║- wa.me/6287892711054
-╠═〘 Hyzer 〙 ═
+║- wa.me/6285759662798
+╠═〘 R α s z Ᏼo͢Ꭲ✿ 〙 ═
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Sewa Bot*', 'status@broadcast') 
