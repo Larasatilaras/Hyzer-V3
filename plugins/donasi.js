43b03b77768986975bcde7cb734c8b7af9f80aff
Laras <96417338+Larasatilaras@users.noreply.github.com>
2022-03-28 15:29:34 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -4,13 +4,13 @@ let handler = async (m, { conn }) => conn.sendButtonLoc(m.chat, await (await fet
 ╭═══════════════
 ║╭─── [ DONASI ] ───
 ║│➸ _*Dana*_✅ : 
-║│- 6282361160044
+║│- 6285759662798
 ║│
 ║│➸ _*Pulsa*_✅: 
-║│- 6285212023766
+║│- 6285759662798
 ║│
 ║│➸ _*OWNER*_
-║│- wa.me/6287892711054
+║│- wa.me/6285759662798
 ║╰────────────
 ╰═══════════════
 `.trim(), 'Donasi jan asal mencet', 'Owner Bot', '.owner')
